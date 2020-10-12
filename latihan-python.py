5c698aa2db84558e2155f5dbb9ed47fd94b51d81
darul-polcep <darulihsanbukik@gmail.com>
2020-10-12 14:06:03 +0700

    pelatihan python 20 oktober 2020

---
+++ latihan-python.py
@@ -0,0 +1,10 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist)
+for x in mylist:
+    print(x)
+print(mylist[2])
+print(mylist[1:2])
+
---
+++ latihan02.py
@@ -0,0 +1,11 @@
+#Contoh pengulangan for sederhana
+angka = [1,2,3,4,5]
+for x in angka:
+    print(x)
+
+#Contoh pengulangan for
+buah = ["nanas", "apel", "jeruk"]
+for makanan in buah:
+    if makanan == "nanas":
+        continue
+    print ("Saya suka makan", makanan)
